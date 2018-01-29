8155de31ef08780b4be3dc2fd508e185e239a565
Curson mañana <Curson maana@PC12a5>
2018-01-29 10:43:29 +0100

    aaaaaa

--- 16_jenifer_eclipsegit/src/basico/Saludar.java
+++ 16_jenifer_eclipsegit/src/basico/Saludar.java
@@ -5,7 +5,7 @@ public class Saludar {
 	public static void main(String[] args) {
 
 		System.out.println("Hola soy Jenifer");
-		System.out.println("Quieres que te pegue un piñaso");
+		System.out.println("Quieres que te pegue un piñasooo");
 
 	}
 
